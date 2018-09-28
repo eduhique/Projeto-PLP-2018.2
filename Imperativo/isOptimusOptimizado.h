6c581f1f8e9a34d7dd000c7c4867a9c39d844c56
Matheus Araújo <39747357+grabowski74@users.noreply.github.com>
2018-09-28 10:02:21 -0300

    Add files via upload

---
+++ Imperativo/isOptimusOptimizado.h
@@ -0,0 +1,14 @@
+/**
+ * Criptografia RSA
+ * Grupo: Eduardo Henrique
+ *        Thayanne Sousa
+ *        Joao Pedro
+ *        Rafael Dantas
+ *        Matheus Araujo
+ **/
+#ifndef isOptimus_h   
+#define isOptimus_h  
+
+void bigPrimeGenerator1(long *p, long *q);
+
+#endif 
