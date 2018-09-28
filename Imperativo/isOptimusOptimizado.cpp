aa5037a30d146e398dc75c9d2a2b73908aecda3f
Matheus Araújo <39747357+grabowski74@users.noreply.github.com>
2018-09-28 10:01:46 -0300

    gerador de numeros primos

---
+++ Imperativo/isOptimusOptimizado.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "isOptimusOptimizado.h"
+using namespace std;
+
+/*
+    @author: Matheus Silva Araujo
+*/
+
+int isPrime(int n)
+{
+    if (n <= 1) return 0;
+    else if (n <= 3) return 1;
+
+    else if (n % 2 == 0 || n % 3 == 0) return 0;
+
+    int i = 5;
+    while (i * i <= n)
+    {
+        if (n % i == 0 || n % (i + 2) == 0) return 0;
+
+        i += 6;
+    }
+    return 1;
+}
+
+/**
+ * Gera dois numeros primos grandes que serao utilizados no algoritmo RSA para gerar as chaves
+ **/
+void bigPrimeGenerator1(long *p, long *q)
+{
+    int numb = 0, times = 0;
+    srand(time(NULL));
+    while (true) {
+        numb = rand() % 9999 + 1000;
+
+        if (numb % 2 != 0) {
+            if (isPrime(numb)) {
+                if (times++ == 0) *p = numb;
+                else {
+                    (*q = numb);
+                    return;
+                }
+            }
+        }
+    }
+}
