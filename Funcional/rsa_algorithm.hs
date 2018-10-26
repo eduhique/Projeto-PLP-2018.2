5bfe3a9e82b9b91699731ab5ab0c9f9d88c6e40d
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-10-26 01:15:19 -0300

    Adição de crip e decrip

--- Funcional/rsa_algorithm.hs
+++ Funcional/rsa_algorithm.hs
@@ -1,35 +1,41 @@
 import Data.List.Split as Split
-import Data.Char as Chardi
+import Data.Char as Chard
 
-modAndPow :: (Int, Int, Int) -> Int
-modAndPow (x, y, z) = x ^ y `mod` z
+modAndPow :: (Integer, Integer, Integer) -> Integer
+modAndPow (x, y, z) = (x ^ y) `mod` z
 
-encrypt_message :: [Int] -> Int -> Int -> [Int]
+encrypt_message :: [Integer] -> Integer -> Integer -> [Integer]
 encrypt_message message e n = [modAndPow(x, e, n) | x <- message]
 
-decrypt_message :: [String] -> Int -> Int -> String
-decrypt_message (num : rest) d n | num == "" = ""
-                                 | otherwise = intToCh (modAndPow (read num, d, n)) ++ decrypt_message rest d n 
+decrypt_message :: [Integer] -> Integer -> Integer -> [Integer]
+decrypt_message encrypted d n = [(modAndPow (x, d, n)) | x <- encrypted]
 
-decrypt_message_int :: [Int] -> Int -> Int -> String
-decrypt_message_int encrypted d n = [intToCh (modAndPow (x, d, n)) | x <- encrypted]
+stringToIntArr :: String -> [Integer]
+stringToIntArr inp = map read (Split.splitOn " " inp)
 
-splitter :: String -> [String]
-splitter inp = Split.splitOn " " inp
--- splitter inp = map read (Split.splitOn " " inp)
+strArr2intArr :: String -> [Integer]
+strArr2intArr message = [toInteger (Chard.ord x) | x <- message]
 
-intToCh :: Int -> String
-intToCh val = [Chardi.chr val]
+-- Inutil
+-- intToCh :: Integer -> String
+-- intToCh val = [Chard.chr val]
 
-f :: [String] -> [Int]
-f = map read
+-- chToint :: Char -> Integer
+-- chToint val = Chard.ord val
+-- \Inutil
 
 main :: IO()
 main = do
-    inp <- getLine;
-    d <- readLn;
-    n <- readLn;
+    
+    inp <- getLine; --Recebe tudo q voce quiser dar
+    d <- readLn;    --d para crip e para decrip
+    n <- readLn;    --n para ambos
 
-    let splitado = splitter inp
+    let zap = strArr2intArr inp -- Recebe a mensagem textual e transforma pra ascii
+    let hol = stringToIntArr inp -- Recebe os números em string e transforma em array de int
+    
+    print (zap)
+    -- print (hol)
 
-    print (decrypt_message splitado d n);
+    print (encrypt_message zap d n);    --Criptografa
+    -- print (dencrypt_message hol d n);    --Decriptografa
