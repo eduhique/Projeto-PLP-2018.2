a40cf3b256f18dd4498f699943a4a4dc67c51196
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-10-26 01:33:26 -0300

    Agora retorna em string \o/

--- Funcional/rsa_algorithm.hs
+++ Funcional/rsa_algorithm.hs
@@ -7,8 +7,8 @@ modAndPow (x, y, z) = (x ^ y) `mod` z
 encrypt_message :: [Integer] -> Integer -> Integer -> [Integer]
 encrypt_message message e n = [modAndPow(x, e, n) | x <- message]
 
-decrypt_message :: [Integer] -> Integer -> Integer -> [Integer]
-decrypt_message encrypted d n = [(modAndPow (x, d, n)) | x <- encrypted]
+decrypt_message :: [Integer] -> Integer -> Integer -> [Char]
+decrypt_message encrypted d n = [intToCh (fromIntegral(modAndPow (x, d, n))) | x <- encrypted]
 
 stringToIntArr :: String -> [Integer]
 stringToIntArr inp = map read (Split.splitOn " " inp)
@@ -16,19 +16,14 @@ stringToIntArr inp = map read (Split.splitOn " " inp)
 strArr2intArr :: String -> [Integer]
 strArr2intArr message = [toInteger (Chard.ord x) | x <- message]
 
--- Inutil
--- intToCh :: Integer -> String
--- intToCh val = [Chard.chr val]
-
--- chToint :: Char -> Integer
--- chToint val = Chard.ord val
--- \Inutil
+intToCh :: Int -> Char
+intToCh val = Chard.chr val
 
 main :: IO()
 main = do
     
     inp <- getLine; --Recebe tudo q voce quiser dar
-    d <- readLn;    --d para crip e para decrip
+    d <- readLn;    --d para decrip e para crip
     n <- readLn;    --n para ambos
 
     let zap = strArr2intArr inp -- Recebe a mensagem textual e transforma pra ascii
@@ -37,5 +32,5 @@ main = do
     print (zap)
     -- print (hol)
 
-    print (encrypt_message zap d n);    --Criptografa
-    -- print (dencrypt_message hol d n);    --Decriptografa
+    -- print (encrypt_message zap d n);    --Criptografa
+    print (decrypt_message hol d n);    --Decriptografa
