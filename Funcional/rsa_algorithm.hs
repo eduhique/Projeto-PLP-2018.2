b126ba8d9eee63696a3c0f11b1ab59db13004f97
Thayanne <thayannevls@gmail.com>
2018-10-25 22:37:42 -0300

    decrypt com array de int

--- Funcional/rsa_algorithm.hs
+++ Funcional/rsa_algorithm.hs
@@ -4,16 +4,15 @@ import Data.Char as Chardi
 modAndPow :: (Int, Int, Int) -> Int
 modAndPow (x, y, z) = x ^ y `mod` z
 
--- encrypt_message :: [Int] -> Int -> Int -> [Int]
--- encrypt_message [] e n = []
--- encrypt_message (c : cs) e n = modAndPow(c, e, n) ++ encrypt_message cs e n
-
 encrypt_message :: [Int] -> Int -> Int -> [Int]
-encrypt_message message e n = [modAndPow(x, e, n)| x <- message]
+encrypt_message message e n = [modAndPow(x, e, n) | x <- message]
 
 decrypt_message :: [String] -> Int -> Int -> String
 decrypt_message (num : rest) d n | num == "" = ""
                                  | otherwise = intToCh (modAndPow (read num, d, n)) ++ decrypt_message rest d n 
+
+decrypt_message_int :: [Int] -> Int -> Int -> String
+decrypt_message_int encrypted d n = [intToCh (modAndPow (x, d, n)) | x <- encrypted]
 
 splitter :: String -> [String]
 splitter inp = Split.splitOn " " inp
