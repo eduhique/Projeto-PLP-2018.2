b9f4d5ad088d86f9de99f03a62d97d060d191991
Thayanne <thayannevls@gmail.com>
2018-11-20 00:15:43 -0300

    ajeita argumentos do encrypt e decrypt

--- Logico/rsaAlgorithm.pl
+++ Logico/rsaAlgorithm.pl
@@ -1,47 +1,17 @@
-% :- initialization main.
+:- initialization main.
 
 stringToAscii(S, L):-
-    atom_codes(L, S).
-
-asciiToString(L, S):-
-<<<<<<< HEAD
-    format("~s~n", [L]).
-
-listToString(L, S):-
-    atomic_list_concat(L, '', Atom),
-    atom_string(Atom, S).
-
-encrypt([], []).
-encrypt([H|T], [X|Result_list]):-
-    X is mod((H ** 3), 391),
-    encrypt(T, Result_list).
-
-decrypt([], []).
-decrypt([H|T], [X|Result_list]):-
-    X is mod((H ** 235), 391),
-    decrypt(T, Result_list).
-
-% main:-
-%     write("Texto: "),
-%     read_line_to_string(user_input, STRING),
-%     stringToAscii(STRING, LISTA),
-%
-%     encrypt(LISTA, CODIFICADA),
-%     write('codificada: '), write(CODIFICADA), write('\n'),
-%
-%     decrypt(CODIFICADA, DECODIFICADA),
-%     listToString(DECODIFICADA, RESULTADO_NUM),
-%     asciiToString(RESULTADO_NUM, RESULTADO_STRING),
-%     write('decodificada: '), write(RESULTADO_STRING).
-=======
     atom_codes(S, L).
 
-encrypt([], [], E, N).
+asciiToString(L, S):-
+    atom_codes(S, L).
+
+encrypt([], [], _, _).
 encrypt([H|T], [X|Result_list], E, N):- 
-    X is mod((H ** E), N),
+    X is mod((H **  E), N),
     encrypt(T, Result_list, E, N).
 
-decrypt([], [], D, N).
+decrypt([], [], _, _).
 decrypt([H|T], [X|Result_list], D, N):- 
     X is mod((H ** D), N),
     decrypt(T, Result_list, D, N).
@@ -58,4 +28,3 @@ main:-
     decrypt(CODIFICADA, DECODIFICADA, 20404259, 48087047),
     asciiToString(DECODIFICADA, RESULTADO_STRING),
     write('decodificada: '), write(RESULTADO_STRING).
->>>>>>> 5e97051fef36ceffcce993e97b360b780d7bb12c
