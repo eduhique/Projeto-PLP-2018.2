6f0a9728293951e95789c7527a399a39c42998cb
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-11-19 23:57:30 -0300

    Atualização do menu

--- Logico/basicOne.pl
+++ Logico/basicOne.pl
@@ -1,7 +1,7 @@
 :- module(
-    'basicOne', 
+    'basicOne',
     [basicOne/1,
-    isPrime2/1]
+    isPrime/1]
 ).
 
 basicOne(Result) :-
@@ -9,25 +9,25 @@ basicOne(Result) :-
     test(A,Result1), Result is Result1.
 
 test(A, Result) :-
-        isPrime2(A) ->
+        isPrime(A) ->
         Result is A;
         random(1000, 20000, B),
         test(B,Result).
 
-isPrime2(2) :-
+isPrime(2) :-
     !.
-isPrime2(3) :-
+isPrime(3) :-
     !.
-isPrime2(X) :-
+isPrime(X) :-
     X > 3,
     X mod 2 =\= 0,
     N_max is ceiling(sqrt(X)),
-    isPrime2_(X,3,N_max).
+    isPrime_(X,3,N_max).
 
-isPrime2_(X,N,N_max) :-
+isPrime_(X,N,N_max) :-
     (  N > N_max
     -> true
     ;  0 =\= X mod N,
        M is N + 2,
-       isPrime2_(X,M,N_max)
+       isPrime_(X,M,N_max)
     ).
--- Logico/rsaAlgorithm.pl
+++ Logico/rsaAlgorithm.pl
@@ -1,34 +1,34 @@
-:- initialization main.
+% :- initialization main.
 
 stringToAscii(S, L):-
-    atom_codes(S, L).
+    atom_codes(L, S).
 
 asciiToString(L, S):-
     format("~s~n", [L]).
 
-listToString(L, S):- 
-    atomic_list_concat(L, '', Atom), 
+listToString(L, S):-
+    atomic_list_concat(L, '', Atom),
     atom_string(Atom, S).
 
 encrypt([], []).
-encrypt([H|T], [X|Result_list]):- 
-    X is mod((H ** 3), 2),
+encrypt([H|T], [X|Result_list]):-
+    X is mod((H ** 3), 391),
     encrypt(T, Result_list).
 
 decrypt([], []).
-decrypt([H|T], [X|Result_list]):- 
-    X is mod((H ** 3), 2),
+decrypt([H|T], [X|Result_list]):-
+    X is mod((H ** 235), 391),
     decrypt(T, Result_list).
 
-main:-
-    write("Texto: "),
-    read_line_to_string(user_input, STRING),
-    stringToAscii(STRING, LISTA),
-    
-    encrypt(LISTA, CODIFICADA),
-    write('codificada: '), write(CODIFICADA), write('\n'),
-    
-    decrypt(CODIFICADA, DECODIFICADA),
-    listToString(DECODIFICADA, RESULTADO_NUM),
-    asciiToString(RESULTADO_NUM, RESULTADO_STRING),
-    write('decodificada: '), write(RESULTADO_STRING).
+% main:-
+%     write("Texto: "),
+%     read_line_to_string(user_input, STRING),
+%     stringToAscii(STRING, LISTA),
+%
+%     encrypt(LISTA, CODIFICADA),
+%     write('codificada: '), write(CODIFICADA), write('\n'),
+%
+%     decrypt(CODIFICADA, DECODIFICADA),
+%     listToString(DECODIFICADA, RESULTADO_NUM),
+%     asciiToString(RESULTADO_NUM, RESULTADO_STRING),
+%     write('decodificada: '), write(RESULTADO_STRING).
